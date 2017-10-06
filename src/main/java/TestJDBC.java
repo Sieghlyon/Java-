2820f149a41cf66c9a68e9b2e4aece62907e1165
Condom Benjamin <benjamin.condom97@ynov.com>
2017-10-06 14:15:05 +0200

    first commit

---
+++ src/main/java/TestJDBC.java
@@ -0,0 +1,10 @@
+
+public class TestJDBC {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ClientManager.loadClientByID(1);
+		ClientManager.createClient("françois_emile", "chaud", "flood", "root");
+	}
+
+}
